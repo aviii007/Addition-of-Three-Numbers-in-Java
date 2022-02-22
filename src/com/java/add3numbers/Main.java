d84f1b35619ae5709fa2bd0cc49ec7c56e510f9d
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-02-22 16:35:57 +0530

    initial commit

---
+++ src/com/java/add3numbers/Main.java
@@ -0,0 +1,14 @@
+package com.java.add3numbers;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("The sum of three numbers is: ");
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
